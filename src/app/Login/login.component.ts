5b44f6c132a1204730ad8756b69d37b44017c077
felipesilvad <31076991+felipesilvad@users.noreply.github.com>
2022-09-15 16:47:44 -0300

    first commit

---
+++ src/app/Login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login-component',
+  templateUrl: './login.component.html',
+  styleUrls: []
+})
+export class LoginComponent implements OnInit {
+  title = 'login-component';
+
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    
+  }
+}
